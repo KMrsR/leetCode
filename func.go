e65625ebe30db081ab948210c881879352ef5c4b
Marsel <vaako1991@mail.ru>
2025-05-25 15:04:18 +1000

    less 2300

--- func.go
+++ func.go
@@ -1,5 +1,9 @@
 package main
 
+import (
+	"sort"
+)
+
 //-------------------------------------------------------------------------------------------------------------------
 // 374. Guess Number Higher or Lower
 
@@ -57,7 +61,6 @@ func guessNumber(n int) int {
 // successful if the product of their strengths is at least success.
 // Return an integer array pairs of length n where pairs[i] is the number of
 // potions that will form a successful pair with the ith spell.
-
 // Example 1:
 // Input: spells = [5,1,3], potions = [1,2,3,4,5], success = 7
 // Output: [4,0,3]
@@ -66,9 +69,50 @@ func guessNumber(n int) int {
 // - 1st spell: 1 * [1,2,3,4,5] = [1,2,3,4,5]. 0 pairs are successful.
 // - 2nd spell: 3 * [1,2,3,4,5] = [3,6,9,12,15]. 3 pairs are successful.
 // Thus, [4,0,3] is returned.
+// Example 2:
+// Input: spells = [3,1,2], potions = [8,5,8], success = 16
+// Output: [2,0,2]
+// Explanation:
+// - 0th spell: 3 * [8,5,8] = [24,15,24]. 2 pairs are successful.
+// - 1st spell: 1 * [8,5,8] = [8,5,8]. 0 pairs are successful.
+// - 2nd spell: 2 * [8,5,8] = [16,10,16]. 2 pairs are successful.
+// Thus, [2,0,2] is returned.
+
+func tf2300(s []int, aim int) int {
+	min := 1
+	max := len(s) - 1
+
+	if s[0] >= aim {
+		return len(s)
+	}
+	if s[len(s)-1] < aim {
+		return 0
+	}
+
+	for max >= min {
+		mid := min + (max-min)/2
+		if s[mid] >= aim && s[mid-1] < aim {
+			return len(s) - mid
+		}
+		if s[mid] < aim {
+			min = mid + 1
+		} else {
+			max = mid - 1
+		}
+	}
+	return -1
+}
 
 func successfulPairs(spells []int, potions []int, success int64) []int {
+	res := make([]int, len(spells))
+	sort.Ints(potions)
 
+	for i, v := range spells {
+		threshold := (success + int64(v) - 1) / int64(v)
+		tmp := tf2300(potions, int(threshold))
+		res[i] = tmp
+	}
+	return res
 }
 
 //-------------------------------------------------------------------------------------------------------------------
--- main.go
+++ main.go
@@ -3,5 +3,7 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println(guessNumber(10))
+	// s := []int{1, 2, 3, 4, 5}
+	// fmt.Println(tf2300(s, 3))
+	fmt.Println(successfulPairs([]int{15, 8, 19}, []int{38, 36, 23}, 328))
 }
